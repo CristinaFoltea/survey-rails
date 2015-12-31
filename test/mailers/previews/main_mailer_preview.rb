cb6e1cc19a845a47ed13be86ea5e5c9d22576ac0
cristinafoltea <criss_foltea@yahoo.com>
2015-12-30 20:28:45 -0700

    ready for deployment to heroku

--- test/mailers/previews/main_mailer_preview.rb
+++ test/mailers/previews/main_mailer_preview.rb
@@ -3,7 +3,7 @@ class MainMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/main_mailer/notify_question_author
   def notify_question_author
-    answer = Answer.first
+    answer = Answer.last
     mail = MainMailer.notify_question_author(answer)
   end
 
